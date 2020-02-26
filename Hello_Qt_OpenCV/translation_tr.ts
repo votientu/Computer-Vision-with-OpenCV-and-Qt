c0c398c6ead7f60d3bed5f2b728d0c70445d7561
Tien Tu VO <votientu@gmail.com>
2020-02-26 08:51:13 +0100

    add translation modules

---
+++ Hello_Qt_OpenCV/translation_tr.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr_TR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>Hello_Qt_OpenCV</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="53"/>
+        <source>Input Image :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="79"/>
+        <source>Filter type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="85"/>
+        <source>Median Blur</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="92"/>
+        <source>Gaussian Blur</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="104"/>
+        <source>Output Image :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="130"/>
+        <source>Display Image After Saving</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
